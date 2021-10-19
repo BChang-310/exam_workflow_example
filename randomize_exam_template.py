27228e2b6ee4d7b21c1e56496b2db7086243bdc0
BChang-310 <89481721+BChang-310@users.noreply.github.com>
2021-10-19 17:38:34 -0500

    Update randomize_exam_template.py

--- randomize_exam_template.py
+++ randomize_exam_template.py
@@ -9,7 +9,7 @@ import numpy as np
 import sys
 # np.random.seed(0)
 
-f = open("Dumb_Template.ipynb", 'r')
+f = open("example_exam.ipynb", 'r')
 
 a = f.read()
 f.close()
@@ -20,6 +20,6 @@ a_number = np.random.randint(20, size=3)
 a = a.replace("a21 & a22 & a23", 
               f"{a_number[0]} & {a_number[1]} & {a_number[2]}")
 
-output_file = open('Dumb_Template.ipynb', 'w')
+output_file = open('example_exam.ipynb', 'w')
 output_file.write(a)
 output_file.close()
